314fcabf51e5980ad1c85421c6bd4bad37dcbfe0
evgenyzhernoklev <oldschool127@mail.ru>
2018-02-07 20:03:23 +0300

    Update slider for tablet

--- src/js/app.js
+++ src/js/app.js
@@ -6,16 +6,26 @@
 //= vendor/slick.js
 
 
+
 /*
  * Custom
  */
+
 $(document).ready(function () {
   $('.slider-images').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     fade: true,
-    asNavFor: '.slider-info'
+    asNavFor: '.slider-info',
+    responsive: [
+      {
+        breakpoint: 920,
+        settings: {
+          arrows: true
+        }
+      }
+    ]
   });
 
   $('.slider-info').slick({
@@ -24,6 +34,17 @@ $(document).ready(function () {
     arrows: false,
     vertical: true,
     focusOnSelect: true,
-    asNavFor: '.slider-images'
+    asNavFor: '.slider-images',
+    responsive: [
+      {
+        breakpoint: 920,
+        settings: {
+          slidesToShow: 1,
+          fade: true,
+          dots: true,
+          vertical: false
+        }
+      }
+    ]
   });
 });
