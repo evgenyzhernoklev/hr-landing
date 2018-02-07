3708992dd6a36346639d89aee09f0aac8150e78b
evgenyzhernoklev <oldschool127@mail.ru>
2018-02-07 19:27:46 +0300

    Add slider for desktop

--- src/js/app.js
+++ src/js/app.js
@@ -9,3 +9,21 @@
 /*
  * Custom
  */
+$(document).ready(function () {
+  $('.slider-images').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-info'
+  });
+
+  $('.slider-info').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    vertical: true,
+    focusOnSelect: true,
+    asNavFor: '.slider-images'
+  });
+});
